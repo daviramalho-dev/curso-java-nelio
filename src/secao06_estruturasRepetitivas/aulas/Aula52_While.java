4d17221170511f979f75a89637c8d5786843e6fc
Ramalho-Sites <daviramalho101@gmail.com>
2026-05-11 17:00:12 -0300

    feat(secao6): Aulas 52 e 56 e exercicios 1 a 3

---
+++ src/secao06_estruturasRepetitivas/aulas/Aula52_While.java
@@ -0,0 +1,20 @@
+package secao06_estruturasRepetitivas.aulas;
+
+import java.util.Scanner;
+
+public class Aula52_While {
+    public static void main(String[] args) {
+        Scanner daviScanner = new Scanner(System.in);
+
+        int numero, soma = 0;
+        System.out.println("Digite um numero inteiro ( 0 para parar):");
+        numero = daviScanner.nextInt();
+
+        while (numero != 0) {
+            soma += numero;
+            System.out.println("Digite outro numero inteiro ( 0 para parar):");
+            numero = daviScanner.nextInt();
+        }
+        System.out.println(soma);
+    }
+}
