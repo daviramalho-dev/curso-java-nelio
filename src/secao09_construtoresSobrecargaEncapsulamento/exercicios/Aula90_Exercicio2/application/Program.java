94684f0dcf23f456f43d80544ab8ed426561aa48
daviramalho-dev <daviramalho101@gmail.com>
2026-05-27 17:05:27 -0300

    feat(seçao9): Adiçao do exercicio 2

---
+++ src/secao09_construtoresSobrecargaEncapsulamento/exercicios/Aula90_Exercicio2/application/Program.java
@@ -0,0 +1,55 @@
+package secao09_construtoresSobrecargaEncapsulamento.exercicios.Aula90_Exercicio2.application;
+
+import secao09_construtoresSobrecargaEncapsulamento.exercicios.Aula90_Exercicio2.entities.Employee;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner daviScanner = new Scanner(System.in);
+        /**
+         * Criar uma classe Employee com:
+         * - name
+         * - grossSalary
+         * - tax
+         *
+         * Regras:
+         * - salário líquido deve ser calculado;
+         * - salário líquido NÃO deve ser atributo;
+         * - aumento salarial altera grossSalary.
+         *
+         * Métodos esperados:
+         * - netSalary()
+         * - increaseSalary(double percentage)
+         * - toString()
+         *
+         * O programa deve:
+         * 1. Ler os dados do funcionário;
+         * 2. Mostrar os dados;
+         * 3. Ler uma porcentagem de aumento;
+         * 4. Aplicar o aumento;
+         * 5. Mostrar os dados atualizados.
+        **/
+        System.out.println("Name: ");
+        String name = daviScanner.nextLine();
+        System.out.println("Gross salary: ");
+        double grossSalary = daviScanner.nextDouble();
+        System.out.println("Tax: ");
+        double tax = daviScanner.nextDouble();
+        Employee employee = new Employee(name,grossSalary,tax);
+
+        System.out.println(employee);
+
+        System.out.println("\nWhich percentage to increase salary? ");
+        double percentage = daviScanner.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println("Update data: " + employee);
+
+
+
+
+
+        daviScanner.close();
+    }
+}
---
+++ src/secao09_construtoresSobrecargaEncapsulamento/exercicios/Aula90_Exercicio2/entities/Employee.java
@@ -0,0 +1,25 @@
+package secao09_construtoresSobrecargaEncapsulamento.exercicios.Aula90_Exercicio2.entities;
+
+public class Employee {
+    private String name;
+    private double grossSalary;
+    private double tax;
+
+    public Employee(String name, double grossSalary, double tax) {
+        this.name = name;
+        this.grossSalary = grossSalary;
+        this.tax = tax;
+    }
+
+    public double netSalary() {
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary(double percentage) {
+        grossSalary += grossSalary * percentage / 100;
+    }
+
+    public String toString() {
+        return  name + ", $ " + String.format("%.2f", netSalary());
+    }
+}
