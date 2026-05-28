db9ef0676245edd59d8dd8cb130f738221da00a6
daviramalho-dev <daviramalho101@gmail.com>
2026-05-28 16:55:02 -0300

    feat(seçao9): Adiçao do exercicio 3

---
+++ src/secao09_construtoresSobrecargaEncapsulamento/exercicios/Aula90_Exercio3/application/Program.java
@@ -0,0 +1,54 @@
+package secao09_construtoresSobrecargaEncapsulamento.exercicios.Aula90_Exercio3.application;
+
+import secao09_construtoresSobrecargaEncapsulamento.exercicios.Aula90_Exercio3.entities.Student;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner daviScanner = new Scanner(System.in);
+        /**
+         * EXERCÍCIO 3 - STUDENT
+         * Criar uma classe Student com:
+         * - name
+         * - grade1
+         * - grade2
+         * - grade3
+         *
+         * Regras:
+         * - calcular nota final;
+         * - informar se foi aprovado;
+         * - caso reprovado, informar quantos pontos faltaram.
+         *
+         * Métodos esperados:
+         * - finalGrade()
+         * - missingPoints()
+         * - isApproved()
+         *
+         * Considere aprovação com 60 pontos.
+         **/
+
+        System.out.println("Name: ");
+        String name = daviScanner.nextLine();
+        System.out.println("Grade 1: ");
+        double grade1 = daviScanner.nextDouble();
+        System.out.println("Grade 2: ");
+        double grade2 = daviScanner.nextDouble();
+        System.out.println("Grade 3: ");
+        double grade3 = daviScanner.nextDouble();
+
+        Student student = new Student(name, grade1, grade2, grade3);
+
+        System.out.println(student);
+
+        if (student.isApproved()) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAILED");
+            System.out.println("MISSING " + String.format("%.2f", student.missingPoints())
+                    + " POINTS"
+            );
+        }
+        daviScanner.close();
+    }
+}
---
+++ src/secao09_construtoresSobrecargaEncapsulamento/exercicios/Aula90_Exercio3/entities/Student.java
@@ -0,0 +1,39 @@
+package secao09_construtoresSobrecargaEncapsulamento.exercicios.Aula90_Exercio3.entities;
+
+public class Student {
+    private String name;
+    private double grade1;
+    private double grade2;
+    private double grade3;
+
+    public Student(String name, double grade1, double grade2, double grade3) {
+        this.name = name;
+        this.grade1 = grade1;
+        this.grade2 = grade2;
+        this.grade3 = grade3;
+    }
+
+    public double finalGrade() {
+        return grade1 + grade2 + grade3;
+    }
+
+    public double missingPoints() {
+        if (!isApproved()) {
+            return 60 - finalGrade();
+        }else  {
+            return 0;
+        }
+    }
+
+    public boolean isApproved() {
+        if (finalGrade() >= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "FINAL GRADE = " + String.format("%.2f", finalGrade());
+    }
+}
